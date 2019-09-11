3b88cb8ecd5880fa4a490c3128b078221eb177a3
WOJCIECH KUBIAK <WGKUBIAK@gmail.com>
2019-09-11 12:02:44 +0200

    Database update

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,18 @@ import Pens from './components/containers/Pens';
 import Test from "./components/containers/Test";
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      pensResponse: [],
+      pen_id: '1'
+    };
+  }
+
   render() {
     return (
       <div className="container">
-        {<Pens />}
+        {<Pens pen_id={this.state.pen_id} />}
       </div>
     );
   }

c2deb4abe890215a56866fddc686f53ffefc32d8
wgkubiak <wgkubiak@gmail.com>
2019-09-21 12:34:12 +0200

    Database insert and delete update - id must autoincrement

--- db/vet.sql
+++ db/vet.sql
@@ -15,7 +15,9 @@ CREATE TABLE global_measures(
     global_wetness INTEGER NOT NULL
 );
 
-INSERT INTO global_measures VALUES (1, '2019-09-17', '18:30:00', 4, 3, 2, 23, 55);
+INSERT INTO global_measures (global_measure_date, global_measure_time,
+global_nh_three, global_h_two_s, global_co_two, global_temperature,
+global_wetness) VALUES ('2019-09-17', '18:30:00', 4, 3, 2, 23, 55);
 
 ---------------- KOJEC --------------------
 CREATE TABLE pen(
@@ -23,7 +25,7 @@ CREATE TABLE pen(
     pen_area_size FLOAT(3) NOT NULL
 );
 
-INSERT INTO pen VALUES (1, 8.56);
+INSERT INTO pen(pen_area_size) VALUES (8.56);
 
 ---------------- POMIAR --------------------
 CREATE TABLE pen_measures(
@@ -36,10 +38,12 @@ CREATE TABLE pen_measures(
     pen_forage INTEGER NOT NULL,
     pen_forage_qty_used FLOAT(5) DEFAULT NULL,
     pen_water SMALLINT NOT NULL,
-    FOREIGN KEY (id_pen) REFERENCES pen(pen_id) -- ON DELETE CASCADE
+    FOREIGN KEY (id_pen) REFERENCES pen(pen_id) ON DELETE CASCADE
 );
 
-INSERT INTO pen_measures VALUES (1, '2019-09-17', '19:00:00', 'Uszkodzone poidlo', NULL, NULL, 1, 14.9, 45);
+INSERT INTO pen_measures(id_pen, pen_measure_date, pen_measure_time, pen_breakdown,
+pen_dosatron, pen_dosatron_addition, pen_forage, pen_forage_qty_used, pen_water)
+VALUES (1, '2019-09-17', '19:00:00', 'Uszkodzone poidlo', NULL, NULL, 1, 14.9, 45);
 
 ----------------- PASZA --------------------
 CREATE TABLE forage(
@@ -50,10 +54,13 @@ CREATE TABLE forage(
     forage_creation_date DATE NOT NULL,
     forage_producer TEXT NOT NULL,
     forage_expiration_date DATE NOT NULL,
-    FOREIGN KEY (id_pen) REFERENCES pen(pen_id) -- ON DELETE CASCADE
+    FOREIGN KEY (id_pen) REFERENCES pen(pen_id) ON DELETE CASCADE
 );
 
-INSERT INTO forage VALUES (1, 'Pasza dedykowana trzodzie chlewnej z dodatkiem witamin i mineralow', 1000, 4.5, '2019-01-01', 'Pasnik', '2020-11-01');
+INSERT INTO forage (id_pen, forage_about, forage_qty, forage_price, forage_creation_date,
+forage_producer, forage_expiration_date)
+VALUES (1, 'Pasza dedykowana trzodzie chlewnej z dodatkiem witamin i mineralow',
+  1000, 4.5, '2019-01-01', 'Pasnik', '2020-11-01');
 
 ----------------- ŚWINIE ------------------
 CREATE TABLE pigs(
@@ -67,10 +74,12 @@ CREATE TABLE pigs(
     pig_sale_date DATE DEFAULT NULL,
     pig_selling_cost FLOAT(10) DEFAULT NULL,
     pig_death_date DATE DEFAULT NULL,
-    FOREIGN KEY (id_pen) REFERENCES pen(pen_id) -- ON DELETE CASCADE
+    FOREIGN KEY (id_pen) REFERENCES pen(pen_id) ON DELETE CASCADE
 );
 
-INSERT INTO pigs VALUES (1, 1, 12345, 'Samiec', NULL, '2019-08-05', 400.99, '2019-09-04', 650.00, NULL);
+INSERT INTO pigs (id_pen, pig_number, pig_gender, rfid, pig_shopping_date,
+pig_shopping_price, pig_sale_date, pig_selling_cost, pig_death_date)
+VALUES ( 1, 12345, 'Samiec', NULL, '2019-08-05', 400.99, '2019-09-04', 650.00, NULL);
 
 ----------------- BADANIE ------------------
 CREATE TABLE examinations(
@@ -90,7 +99,11 @@ CREATE TABLE examinations(
     lameness ZERO_TO_TWO_RATE NOT NULL,
     respiratory_system ZERO_TO_THREE_RATE NOT NULL,
     skin_changes TEXT DEFAULT NULL,
-    FOREIGN KEY (number_pig) REFERENCES pigs(pig_number) -- ON DELETE CASCADE
+    FOREIGN KEY (number_pig) REFERENCES pigs(pig_number) ON DELETE CASCADE
 );
 
-INSERT INTO examinations VALUES (12345, 1, '2019-09-17', '20:00:00', NULL, NULL, NULL, NULL, NULL, NULL, '1', 64.5, 38.5, '1', '2', NULL);
+INSERT INTO examinations (number_pig, exam_date, exam_time, feces, tissue,
+exam_result, medicine, medicine_qty, medicine_type, diarrhea, weight, temperature,
+lameness, respiratory_system, skin_changes)
+VALUES (12345, '2019-09-17', '20:00:00', NULL, NULL, NULL, NULL, NULL, NULL,
+  '1', 64.5, 38.5, '1', '2', NULL);
